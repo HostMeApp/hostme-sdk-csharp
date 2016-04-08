ba6810457670594826be237076d7c81290d7358a
Evgeny Popov <evgeny.popov@eastbanctech.com>
2016-04-07 17:38:30 -0700

    added solution and project files to all sdk libraries

---
+++ hostme-sdk-csharp-client/Extentions/AccessTokenViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace HostMe.Sdk.Auth
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class AccessTokenViewModel
+    {
+        [DataMember(Name = "access_token")]
+        public string AccessToken { get; set; }
+
+        [DataMember(Name = ".expires")]
+        public DateTime Expires { get; set; }
+
+        [DataMember(Name = ".issued")]
+        public DateTime Issued { get; set; }
+
+        [DataMember(Name = "token_type")]
+        public string TokenType { get; set; }
+
+        [DataMember(Name = "userName")]
+        public string UserName { get; set; }
+    }
+}
